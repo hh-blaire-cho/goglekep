1958b102fb03b1d2823d59a431376b345f8d89fa
Blaire <hh.blaire.cho@gmail.com>
2024-09-18 23:18:47 +0900

    [CSRF] skeleton of post auth action

--- goglekep/__init__.py
+++ goglekep/__init__.py
@@ -23,18 +23,43 @@ def create_app():
 
     from goglekep.forms.auth_form import LoginForm, RegisterForm
 
-    @app.route("/auth/login")
+    @app.route("/auth/login", methods=["GET", "POST"])
     def login():
         form = LoginForm()
+        if form.validate_on_submit():
+            # TODO: impl list below
+            # 1) 유저조회
+            # 2) 존재하는 유저 인지 체크
+            # 3) 패스워드 정합확인
+            # 3) 로그인 유지(세션)
+            user_id = form.data.get('user_id')
+            password = form.data.get('password')
+            return f'ID: {user_id}</br>PW: {password}'
+        else:
+            pass  # TODO: 에러컨트롤
         return render_template('login.html', form=form)
 
-    @app.route("/auth/register")
+    @app.route("/auth/register", methods=["GET", "POST"])
     def register():
         form = RegisterForm()
+        if form.validate_on_submit():
+            # TODO: impl list below
+            # 1) 유저조회
+            # 2) 유저 이미 존재하는지 체크
+            # 3) 없으면 유저 생성
+            # 4) 로그인 유지(세션)
+            user_id = form.data.get('user_id')
+            password = form.data.get('password')
+            repassword = form.data.get('repassword')
+            nickname = form.data.get('nickname')
+            return f'ID: {user_id}</br>PW: {password}</br>NICKNAME: {nickname}'
+        else:
+            pass  # TODO: 에러컨트롤
         return render_template('register.html', form=form)
 
     @app.route("/auth/logout")
     def logout():
+        # TODO: impl
         return "logging out..."
 
     return app
